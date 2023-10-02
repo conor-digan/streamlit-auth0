4efc42e8a2c366693d5ec36074aee9341d02a342
conor-digan <conordigan@gmail.com>
2023-10-02 15:28:42 +0100

    Changed the __init__.py to check for eu domains NOT us ones

--- auth0_component/__init__.py
+++ auth0_component/__init__.py
@@ -23,8 +23,8 @@ from jose import jwt
 
 def getVerifiedSubFromToken(token, domain):
     domain = "https://"+domain
-    if domain[-13:] != '.us.auth0.com':
-        print('domain should end with ".us.auth0.com" (no slash)')
+    if domain[-13:] != '.eu.auth0.com':
+        print('domain should end with ".eu.auth0.com" (no slash)')
         raise ValueError
     jsonurl = urlopen(domain+"/.well-known/jwks.json")
     jwks = json.loads(jsonurl.read())
